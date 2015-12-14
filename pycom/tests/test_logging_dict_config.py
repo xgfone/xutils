eebb025f4466a5c26bc99f9a1f24a5f14ca14387
xgfone <xgfone@126.com>
2015-12-14 09:50:16 +0800

    Add the test of `logging_dict_config`.

---
+++ pycom/tests/test_logging_dict_config.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+import unittest
+from pycom.logging_dict_config import get_config
+
+
+class LoggingTests(unittest.TestCase):
+
+    def test_get_config_project(self):
+        config = get_config("project")
+        left = "/var/log/project/project.log"
+        right = config["handlers"]["file"]["filename"]
+        self.assertEqual(left, right)
+
+    def test_get_config_config(self):
+        conf = {
+            "root": {
+                "level": "INFO",
+            }
+        }
+        config = get_config("project", config=conf)
+        self.assertEqual("INFO", config["root"]["level"])
