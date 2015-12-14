a67c997a13761b4a88238d2984cc3a40257ef4e9
xgfone <xgfone@126.com>
2015-12-14 09:47:13 +0800

    Add the test of `objects`.

---
+++ pycom/tests/test_objects.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+import unittest
+from pycom.objects import AttrWrapper
+
+
+class ObjectsTests(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(ObjectsTests, self).__init__(*args, **kwargs)
+
+    def test_attr_wrapper_yes(self):
+        class TestAttrWrapper(AttrWrapper):
+            attrs = ["attr"]
+        obj = TestAttrWrapper()
+        obj.attr = "attr"
+        self.assertEquals(obj.attr, "attr")
+
+    def test_attr_wrapper_no(self):
+        class TestAttrWrapper(AttrWrapper):
+            attrs = []
+        obj = TestAttrWrapper()
+        with self.assertRaises(AttributeError):
+            obj.attr = "attr"
