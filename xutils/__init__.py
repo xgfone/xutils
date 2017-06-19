271f16e4f3030aab17ce05ed00753f9cdebc6885
xiegaofeng <xiegaofeng@grandcloud.cn>
2017-06-19 09:45:50 +0800

    Remove the builtin

--- xutils/__init__.py
+++ xutils/__init__.py
@@ -6,24 +6,3 @@ if sys.version_info[0] == 2:
     PY3 = False
 else:
     PY3 = True
-
-
-try:
-    from pycom.builtin import set_builtin
-except ImportError:
-    if PY3:
-        import builtins
-    else:
-        import __builtin__ as builtins
-
-    def set_builtin(name, value, force=False):
-        try:
-            getattr(builtins, name)
-        except AttributeError:
-            pass
-        else:
-            if not force:
-                raise AttributeError("{0} has already existed".format(name))
-        setattr(builtins, name, value)
-finally:
-    set_builtin("PY3", PY3)
