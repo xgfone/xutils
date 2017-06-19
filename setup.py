62b453e3be5a3503eb03863b8ee5ecbf775417f7
xiegaofeng <xiegaofeng@grandcloud.cn>
2017-06-19 11:32:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import os.path
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+README_FILE = os.path.join(ROOT_DIR, "README.rst")
+
+with open(README_FILE) as f:
+    long_description = f.read()
+
 setup(
     name="xutils",
-    version="0.8.1",
+    version="0.8.2",
     description="A Fragmentary Python Library.",
+    long_description=long_description,
     author="xgfone",
     author_email="xgfone@126.com",
     maintainer="xgfone",
