6fb99d038f7c64175f6709ddba638b6be0c1c030
xiegaofeng <xiegaofeng@grandcloud.cn>
2017-06-19 09:47:36 +0800

    Add the compatible types

--- xutils/__init__.py
+++ xutils/__init__.py
@@ -3,6 +3,13 @@ import sys
 
 # On Python(>=2.7), sys.version_info[0] <==> sys.version_info.major
 if sys.version_info[0] == 2:
-    PY3 = False
+    PY2, PY3 = True, False
+    byte_type, text_type, string_type = str, unicode, basestring
 else:
-    PY3 = True
+    PY2, PY3 = False, True
+    byte_type, text_type, string_type = bytes, str, str
+
+Bytes, Unicode, String = byte_type, text_type, string_type
+to_bytes = lambda v, e="utf-8": v.encode(e) if isinstance(v, Unicode) else v
+to_unicode = lambda v, e="utf-8": v.decode(e) if isinstance(v, Bytes) else v
+to_str = to_unicode if PY3 else to_bytes
