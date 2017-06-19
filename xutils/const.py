85efa9c105ddb9240a25be433de76ef21b3ed2b3
xiegaofeng <xiegaofeng@grandcloud.cn>
2017-06-19 11:08:46 +0800

    Fix a bug on Python 3.6

--- xutils/const.py
+++ xutils/const.py
@@ -12,10 +12,10 @@ class _const(object):
 
     def __setattr__(self, name, value):
         if name in self.__dict__:
-            raise self.ConstError, "Can't change const.{0}".format(name)
+            raise self.ConstError("Can't change const.{0}".format(name))
 
         if not name.isupper():
-            raise self.ConstCaseError, 'const name "{0}" is not all uppercase'.format(name)
+            raise self.ConstCaseError('const name "{0}" is not all uppercase'.format(name))
 
         self.__dict__[name] = value
 
