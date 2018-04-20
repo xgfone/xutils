be1d8a71592cdf27f7ae2c7f252e878d6d6c6dda
xgfone <xgfone@126.com>
2018-04-20 18:17:44 +0800

    Add rate limit

---
+++ xutils/rate.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+import time
+
+from threading import Thread
+try:
+    from queue import Queue
+except ImportError:
+    from Queue import Queue
+
+
+class Rate(object):
+    def __init__(self, rate, hz=10):
+        if rate < 1 or hz < 1:
+            raise ValueError("rate and hz must be a positive integer")
+
+        _rate = rate // hz
+        _rate, interval = (_rate, 1.0 / hz) if _rate else (1, 1.0 / rate)
+
+        self._rate = _rate
+        self._interval = interval
+        self._queue = Queue(rate)
+
+        self._thread = Thread(target=self._start)
+        self._thread.daemon = True
+        self._thread.start()
+
+    def _start(self):
+        while True:
+            diff = self._rate - self._queue.qsize()
+            while diff > 0:
+                self._queue.put(None)
+                diff -= 1
+            time.sleep(self._interval)
+
+    def get_token(self):
+        self._queue.get()
+        self._queue.task_done()
