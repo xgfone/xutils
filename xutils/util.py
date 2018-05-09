d8f8d7d2e6b9c7537a4f5cdd2c3b7251017186a3
xgfone <xgfone@126.com>
2018-05-09 17:05:46 +0800

    Add check_output to be compatible with Py2 and Py3

--- xutils/util.py
+++ xutils/util.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from subprocess import check_output
+from subprocess import check_output as _check_output
+from xutils import PY3, to_unicode
+
+
+if PY3:
+    def check_output(cmd, timeout=None, encoding=None, **kwargs):
+        return _check_output(cmd, timeout=timeout, encoding=encoding, **kwargs)
+else:
+    def check_output(cmd, timeout=None, encoding=None, **kwargs):
+        out = _check_output(cmd, **kwargs)
+        return to_unicode(out, encoding) if encoding else out
 
 
 def which(command, which="/usr/bin/which"):
