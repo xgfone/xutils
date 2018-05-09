47304eafa7880da8a54f1e1172bc2dab814238a7
xgfone <xgfone@126.com>
2018-05-09 17:07:13 +0800

    Add new argument options for util.which()

--- xutils/util.py
+++ xutils/util.py
@@ -13,13 +13,16 @@ else:
         return to_unicode(out, encoding) if encoding else out
 
 
-def which(command, which="/usr/bin/which"):
+def which(command, default=None, encoding="utf-8", which="/usr/bin/which"):
     """Find and return the first absolute path of command used by `which`.
 
-    If not found, return "".
+    If not found and default is not None, return default.
+    Or reraise an exception.
     """
 
     try:
-        return check_output([which, command]).split("\n")[0]
+        return to_unicode(check_output([which, command]), encoding).split("\n")[0]
     except Exception:
-        return ""
+        if default is not None:
+            return default
+        raise
