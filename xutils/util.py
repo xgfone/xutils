50251ff7f221dd6953c89602bd86af2df4f0fd91
xgfone <xgfone@126.com>
2018-05-10 10:12:01 +0800

    Change the interface of utils.which()

--- xutils/util.py
+++ xutils/util.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from subprocess import check_output
-from xutils import to_unicode
+from xutils import to_unicode, is_string
 
 
-def which(command, default=None, encoding="utf-8", which="/usr/bin/which"):
-    """Find and return the first absolute path of command used by `which`.
+def which(command, reraise=False, encoding="utf-8", which="/usr/bin/which"):
+    """Find and return the absolute path list of ``command`` used by ``which``.
 
-    If not found and default is not None, return default.
-    Or reraise an exception.
+    If not found, raise a ``RuntimeError`` exception if ``reraise`` is ``True``,
+    or return an empty list.
     """
 
+    if not is_string(command):
+        raise ValueError("the first argument 'command' must be string")
+
     try:
-        return to_unicode(check_output([which, command]), encoding).split("\n")[0]
+        lines = to_unicode(check_output([which, command]), encoding).split("\n")
+        cmds = [line for line in lines if line]
     except Exception:
-        if default is not None:
-            return default
-        raise
+        cmds = []
+
+    if cmds or not reraise:
+        return cmds
+    raise RuntimeError("not found '%s'" % command)
