794894085c2e9f87557b0af1f118fb1ed3850da8
xgfone <xgfone@126.com>
2018-09-04 10:01:22 +0800

    change the implementation of Configuration Group

--- xutils/config.py
+++ xutils/config.py
@@ -39,43 +39,148 @@ def find_config_file(name, dir=None, extra_dirs=("/opt", "/etc")):
     raise RuntimeError("Not found %s" % filename)
 
 
-class Configuration(object):
-    class Group(object):
-        def __init__(self, group_name):
-            self.__name = group_name
+class Option(object):
+    def __init__(self, name, short=None, default=None, help=None, cli=True):
+        self.name = name
+        self.short = short
+        self.default = default
+        self.help = help
+        self.cli = cli
 
-        def __repr__(self):
-            attrs = []
-            for key, value in vars(self).items():
-                if key != "_Group__name":
-                    attrs.append("{0}={1}".format(key, value))
-            return "{0}({1})".format(self.__class__.__name__, ", ".join(attrs))
+    def parse(self, value):
+        return value
 
-        def __contains__(self, name):
-            return hasattr(self, name)
 
-        def __getattr__(self, name):
+class String(Option):
+    try:
+        "".decode()
+    except AttributeError:
+        _py2 = False
+    else:
+        _py2 = True
+
+    def __init__(self, name, short=None, default=None, help=None, cli=True,
+                 encoding='utf-8'):
+        super(String, self).__init__(name, short=short, default=default,
+                                     help=help, cli=cli, encoding=encoding)
+        self._encoding = encoding
+
+    def parse(self, value):
+        if self._py2:
+            if isinstance(value, str):
+                return value.decode(self._encoding)
+        else:
+            if not isinstance(value, str):
+                return value.decode(self._encoding)
+        return value
+
+
+class Bool(Option):
+    TRUE = ["t", "1", "on", "true"]
+    FALSE = ["f", "0", "off", "false"]
+
+    def parse(self, value):
+        if isinstance(value, bool):
+            return value
+        elif not is_string(value):
+            return bool(value)
+
+        value = value.lower()
+        if value in Bool.TRUE:
+            return True
+        elif value in Bool.FALSE:
+            return False
+        raise ValueError("invalid bool value '{0}'".format(value))
+
+
+class Int(Option):
+    def __init__(self, name, short=None, default=None, help=None, cli=True,
+                 base=10):
+        super(Int, self).__init__(name, short=short, default=default, help=help,
+                                  cli=cli)
+        self._base = base
+
+    def parse(self, value):
+        if isinstance(value, int):
+            return value
+        return int(value, self._base)
+
+
+class Float(Option):
+    def parse(self, value):
+        return float(value)
+
+
+class List(Option):
+    def __init__(self, parser, name, short=None, default=None, help=None,
+                 cli=True):
+        super(List, self).__init__(name, short=short, default=default,
+                                   help=help, cli=cli)
+        self._parser = parser
+
+    def parse(self, value):
+        if isinstance(value, (list, tuple)):
+            vs = value
+        else:
+            vs = (v.strip() for v in value.split(",") if v.strip())
+        return tuple((self._parser(v) for v in vs))
+
+
+class IntList(List):
+    def __init__(self, name, short=None, default=None, help=None, cli=True,
+                 base=10):
+        super(IntList, self).__init__(Int("", base=base).parse, name,
+                                      short=short, default=default, help=help,
+                                      cli=cli)
+
+
+class FloatList(List):
+    def __init__(self, name, short=None, default=None, help=None, cli=True):
+        super(FloatList, self).__init__(Float("", cli=cli).parse, name,
+                                        short=short, default=default,
+                                        help=help, cli=cli)
+
+
+class StringList(List):
+    def __init__(self, name, short=None, default=None, help=None, cli=True,
+                 encoding='utf-8'):
+        super(StringList, self).__init__(String("", encoding=encoding).parse,
+                                         name, short=short, default=default,
+                                         help=help, cli=cli)
+
+
+class Group(object):
+    def __init__(self, group_name):
+        self.__name = group_name
+
+    def __repr__(self):
+        attrs = ("{0}={1}".format(k, v) for k, v in vars(self).items())
+        return "{0}({1})".format(self.__class__.__name__, ", ".join(attrs))
+
+    def __contains__(self, name):
+        return hasattr(self, name)
+
+    def __getattr__(self, name):
+        e = "The group '{0}' has no the option '{1}'"
+        raise AttributeError(e.format(self.__name, name))
+
+    def __setitem__(self, name, value):
+        setattr(self, name, value)
+
+    def __getitem__(self, name):
+        try:
+            return getattr(self, name)
+        except AttributeError:
             e = "The group '{0}' has no the option '{1}'"
-            raise AttributeError(e.format(self.__name, name))
+            raise KeyError(e.format(self.__name, name))
 
-        def __setitem__(self, name, value):
-            setattr(self, name, value)
+    def items(self):
+        return vars(self).items()
 
-        def __getitem__(self, name):
-            try:
-                return getattr(self, name)
-            except AttributeError:
-                e = "The group '{0}' has no the option '{1}'"
-                raise KeyError(e.format(self.__name, name))
 
-        def items(self):
-            d = vars(self)
-            d.pop("_Group__name")
-            return d.items()
-
+class Configuration(object):
     __slots__ = ["_default_group_name", "_default_group", "_allow_empty",
-                 "_encoding", "_parsed", "_caches", "_opts", "_bool_true",
-                 "_bool_false", "_py2", "_description", "_version"]
+                 "_parsed", "_caches", "_opts", "_description", "_version"]
 
     def __init__(self, description=None, allow_empty=False, encoding="utf-8",
                  default_group="DEFAULT", version=None):
@@ -89,23 +194,12 @@ class Configuration(object):
         self._parsed = False
         self._description = description
         self._default_group_name = default_group
-        self._default_group = Configuration.Group(self._default_group_name)
+        self._default_group = Group(self._default_group_name)
         self._allow_empty = allow_empty
-        self._encoding = encoding
         self._version = version if version else "Unknown"
 
         self._caches = {self._default_group_name: self._default_group}
         self._opts = {}
-
-        self._bool_true = ["t", "1", "on", "true"]
-        self._bool_false = ["f", "0", "off", "false"]
-
-        try:
-            "".decode()
-        except AttributeError:
-            self._py2 = False
-        else:
-            self._py2 = True
 
     def __getattr__(self, name):
         if not self._parsed:
@@ -133,9 +227,7 @@ class Configuration(object):
         raise KeyError(msg.format(self.__class__.__name__, name))
 
     def __repr__(self):
-        attrs = []
-        for key, value in self._caches.items():
-            attrs.append("{0}={1}".format(key, value))
+        attrs = ("{0}={1}".format(k, v) for k, v in self._caches.items())
         return "{0}({1})".format(self.__class__.__name__, ", ".join(attrs))
 
     def _set_group_opt(self, group_name, opt_name, opt_value, force=False):
@@ -145,61 +237,6 @@ class Configuration(object):
             e = "The group '{0}' has had the option of '{1}'"
             raise ValueError(e.format(gname, opt_name))
         setattr(self._caches[gname], opt_name, opt_value)
-
-    def _register(self, name, parser, default=None, group=None, help=None, short=None):
-        if self._parsed:
-            raise Exception("Have been parsed")
-
-        name = self._uniformize(name)
-        group = self._uniformize(group if group else self._default_group_name)
-        self._opts.setdefault(group, {})
-
-        if name in self._opts[group]:
-            raise KeyError("The option {0} has been regisetered".format(name))
-
-        self._opts[group][name] = (parser, default, help, short)
-        self._caches.setdefault(group, Configuration.Group(group))
-
-    def _parse_int(self, value):
-        return int(value)
-
-    def _parse_float(self, value):
-        return float(value)
-
-    def _parse_bool(self, value):
-        if isinstance(value, bool):
-            return value
-        elif not is_string(value):
-            return bool(value)
-
-        value = value.lower()
-        if value in self._bool_true:
-            return True
-        elif value in self._bool_false:
-            return False
-        raise ValueError("invalid bool value '{0}'".format(value))
-
-    def _parse_string(self, value):
-        if self._py2:
-            if isinstance(value, str):
-                return value.decode(self._encoding)
-        else:
-            if not isinstance(value, str):
-                return value.decode(self._encoding)
-        return value
-
-    def _parse_ints(self, value):
-        return self._parse_list(self._parse_int, value)
-
-    def _parse_strings(self, value):
-        return self._parse_list(self._parse_string, value)
-
-    def _parse_list(self, parser, value):
-        if isinstance(value, (list, tuple)):
-            vs = value
-        else:
-            vs = (v.strip() for v in value.split(",") if v.strip())
-        return tuple((parser(v) for v in vs))
 
     def _uniformize(self, name):
         return name.replace("-", "_")
@@ -236,8 +273,8 @@ class Configuration(object):
             for name, opt in opts.items():
                 if name in group:
                     continue
-                elif opt[1] is not None or opt[0] == self._parse_bool:
-                    self._set_group_opt(gname, name, opt[1])
+                elif opt.default is not None or issubclass(opt, Bool):
+                    self._set_group_opt(gname, name, opt.default)
                     continue
 
                 if not self._allow_empty:
@@ -303,53 +340,93 @@ class Configuration(object):
             if opt:
                 self._set_group_opt(gname, name, opt[0](value))
 
-    def register_bool(self, name, short=None, default=None, group=None, help=None):
+    def register_opt(self, opt, group=None):
+        if not isinstance(opt, Option):
+            raise ValueError("the option must be the subclass of Option")
+
+        if self._parsed:
+            raise Exception("Have been parsed")
+
+        name = self._uniformize(opt.name)
+        group = self._uniformize(group or self._default_group_name)
+        self._opts.setdefault(group, {})
+
+        if name in self._opts[group]:
+            raise KeyError("The option {0} has been regisetered".format(name))
+
+        self._opts[group][name] = opt
+        self._caches[group] = Group(group)
+
+    def register_opts(self, opts, group=None):
+        for opt in opts:
+            self.register_opt(opt, group=group)
+
+    def register_bool(self, name, short=None, default=None, group=None,
+                      help=None, cli=True):
         """Register the bool option.
 
         The value of this option will be parsed to the type of bool.
         """
-        self._register(name, self._parse_bool, short=short, default=default,
-                       group=group, help=help)
+        opt = Bool(name, short=short, default=default, help=help, cli=cli)
+        self.register_opt(opt, group=group)
 
-    def register_int(self, name, short=None, default=None, group=None, help=None):
+    def register_int(self, name, short=None, default=None, group=None,
+                     help=None, cli=True, base=10):
         """Register the int option.
 
         The value of this option will be parsed to the type of int.
         """
-        self._register(name, self._parse_int, short=short, default=default,
-                       group=group, help=help)
+        opt = Int(name, short=short, default=default, help=help, cli=cli,
+                  base=base)
+        self.register_opt(opt, group=group)
 
-    def register_float(self, name, short=None, default=None, group=None, help=None):
+    def register_float(self, name, short=None, default=None, group=None,
+                       help=None, cli=True):
         """Register the float option.
 
         The value of this option will be parsed to the type of float.
         """
-        self._register(name, self._parse_float, short=short, default=default,
-                       group=group, help=help)
+        opt = Float(name, short=short, default=default, help=help, cli=cli)
+        self.register_opt(opt, group=group)
 
-    def register_str(self, name, short=None, default=None, group=None, help=None):
+    def register_str(self, name, short=None, default=None, group=None,
+                     help=None, cli=True, encoding='utf-8'):
         """Register the str option.
 
         The value of this option will be parsed to the type of str.
         """
-        self._register(name, self._parse_string, short=short, default=default,
-                       group=group, help=help)
+        opt = String(name, short=short, default=default, help=help, cli=cli,
+                     encoding=encoding)
+        self.register_opt(opt, group=group)
 
-    def register_int_list(self, name, short=None, default=None, group=None, help=None):
+    def register_int_list(self, name, short=None, default=None, group=None,
+                          help=None, cli=True, base=10):
         """Register the int list option.
 
         The value of this option will be parsed to the type of int list.
         """
-        self._register(name, self._parse_ints, short=short, default=default,
-                       group=group, help=help)
+        opt = IntList(name, short=short, default=default, help=help, cli=cli,
+                      base=base)
+        self.register_opt(opt, group=group)
 
-    def register_str_list(self, name, short=None, default=None, group=None, help=None):
+    def register_str_list(self, name, short=None, default=None, group=None,
+                          help=None, cli=True, encoding='utf-8'):
         """Register the string list option.
 
         The value of this option will be parsed to the type of string list.
         """
-        self._register(name, self._parse_strings, short=short, default=default,
-                       group=group, help=help)
+        opt = StringList(name, short=short, default=default, help=help,
+                         cli=cli, encoding=encoding)
+        self.register_opt(opt, group=group)
+
+    def register_float_list(self, name, short=None, default=None, group=None,
+                            help=None, cli=True):
+        """Register the float list option.
+
+        The value of this option will be parsed to the type of float list.
+        """
+        opt = FloatList(name, short=short, default=default, help=help, cli=cli)
+        self.register_opt(opt, group=group)
 
     ###########################################################################
     # Parse CLI
@@ -385,14 +462,14 @@ class Configuration(object):
             opt = self._opts[gname][name]
             value = getattr(args, cli_opt, None)
             if value is not None:
-                value = opt[0](value)
-                if opt[0] == self._parse_bool:
-                    if opt[1] is None:
+                value = opt.parse(value)
+                if issubclass(opt, Bool):
+                    if opt.default is None:
                         if value:
                             self._set_group_opt(gname, name, value, force=True)
-                    elif value != opt[1]:
+                    elif value != opt.default:
                         self._set_group_opt(gname, name, value, force=True)
-                elif value != opt[1]:
+                elif value != opt.default:
                     self._set_group_opt(gname, name, value, force=True)
 
         self._check_and_fix()
@@ -413,11 +490,14 @@ class Configuration(object):
             else:
                 group = cli.add_argument_group(gname)
 
-            for name, (parser, default, help, short) in opts.items():
+            for name, opt in opts.items():
+                if not opt.cli:
+                    continue
+
                 action = None
-                if parser == self._parse_bool:
-                    action = "store_false" if default else "store_true"
-                    default = False if default is None else default
+                if issubclass(opt, Bool):
+                    action = "store_false" if opt.default else "store_true"
+                    default = False if opt.default is None else opt.default
 
                 if gname == self._default_group_name:
                     opt_name = self._unniformize(name)
@@ -426,8 +506,10 @@ class Configuration(object):
                     opt_name = self._unniformize("{0}-{1}".format(gname, name))
                     opt_key = self._uniformize(opt_name)
                 group_opts[opt_key] = (gname, name)
+                short = opt.short
                 short = "-" + short if short and short[0] != "-" else short
                 names = [short, "--" + opt_name] if short else ["--" + opt_name]
-                group.add_argument(*names, action=action, default=default, help=help)
+                group.add_argument(*names, action=action, default=default,
+                                   help=opt.help)
 
         return group_opts, cli.parse_args(args=args)
